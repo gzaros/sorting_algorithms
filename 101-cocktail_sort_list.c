a99901fac221ebc3e3d4cfb8fb37fad7cdd5ba5b
gzaros <templatewebvo@gmail.com>
2023-09-23 08:03:35 -0700

    sorting_algorithms

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,91 @@
+#include "sort.h"
+/**
+ * Swap the positions of two nodes in a doubly-linked list.
+ *
+ * @a: Address of the first node to swap.
+ * @b: Address of the second node to swap.
+ *
+ * Return: void
+ */
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+/*
+ * tail_traverse - A function that sorts the list in reverse order, starting from the tail.
+ *
+ * @head: The head of the list.
+ * @tail: The tail of the list.
+ * @list: The original head of the list.
+ *
+ * Return: The new head of the list.
+ */
+listint_t *tail_traverse(listint_t *head, listint_t *tail, listint_t *list)
+{
+	while (tail && tail->prev)
+	{
+		if (tail->n < tail->prev->n)
+		{
+			swap(tail->prev, tail);
+			if (tail->prev == NULL)
+				list = tail;
+			print_list(list);
+		}
+		else
+			tail = tail->prev;
+		if (tail->prev == NULL)
+			head = tail;
+	}
+	return (head);
+}
+
+/**
+ * cocktailSortList - Sorts a doubly linked list using cocktail shaker sort.
+ *
+ * @list: Pointer to the doubly linked list to be sorted.
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *tail, *head, *len;
+	int i = 0, j = 0, swaped = 1;
+
+	if (!list || !*list)
+		return;
+	len = *list;
+	for (i = 0; len; i++)
+	{
+		len = len->next;
+	}
+	if (i < 2)
+		return;
+	head = *list;
+	while (j < i)
+	{
+		swaped = 0;
+		while (head && head->next)
+		{
+			if (head->n > head->next->n)
+			{
+				swap(head, head->next);
+				swaped++;
+				if (head->prev->prev == NULL)
+					*list = head->prev;
+				print_list(*list);
+			}
+			else
+				head = head->next;
+			if (head->next == NULL)
+				tail = head;
+		}
+		head = tail_traverse(head, tail, *list);
+		*list = head;
+		j++;
+	}
+}
